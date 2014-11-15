c14dca5997615fae7a5d1c93e1408ebbb329b9a7
Fatumner <Fatumner@wp.pl>
2014-11-15 05:06:50 +0100

    added menu scene (properties, games etc) for now entering another scene (game) is working, and moving in the menu (when having menu in front and pressing up and down buttons) press escape in game to go back to menu. Investigate errors when pressing escape (not always occuring o_O)

---
+++ Assets/scripts/GameMenuController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenuController : MonoBehaviour {
+
+	public string gameName;
+	public string[] menuItems;
+	//public GameObject gameMenu;
+	public GameObject text;
+	GameObject[] textItems;
+	TextMesh[] meshes;
+	int selectedIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		meshes = new TextMesh[menuItems.Length];
+		textItems = new GameObject[menuItems.Length];
+		float distanceToFloor = 1f;
+		float offset = distanceToFloor + menuItems.Length * 1.5f + 0.5f;
+		this.transform.localScale = new Vector3(this.transform.localScale.x, menuItems.Length * 1.5f + 2f, this.transform.localScale.z);
+		this.transform.position = new Vector3(this.transform.position.x, this.transform.localScale.y / 2f + distanceToFloor, this.transform.position.z);
+		
+		//Vector3 position = this.transform.position;
+		Quaternion q = this.transform.rotation; //new Quaternion(0f, 180f, 0f, 1f);
+		for (int i = 0; i < menuItems.Length; ++i) {
+			Vector3 v = new Vector3 (this.transform.position.x , offset, this.transform.position.z);
+			//(text.GetComponent("TextMesh") as TextMesh).text = menuItems[i];
+			//(text.GetComponent("TextMesh") as TextMesh).color = Color.red;
+			//textItems[i] = text;
+
+
+			/*if(i == 0) {
+				text.GetComponent<TextMesh>().font.material.color = Color.red;
+			} else if(i == 1) {
+				text.GetComponent<TextMesh>().font.material.color = Color.green;
+			} else {
+				text.GetComponent<TextMesh>().font.material.color = Color.blue;
+			}*/
+			print (text.GetComponent<TextMesh>().text);
+			textItems[i] = (GameObject) Instantiate (text, v, q);
+			DestroyImmediate(textItems[i].GetComponent<TextMesh>());
+			print (text.GetComponent<TextMesh>().text);
+			meshes[i] = textItems[i].AddComponent<TextMesh>();
+			TextMesh tmp = text.GetComponent<TextMesh>();
+			meshes[i].alignment = tmp.alignment;
+			meshes[i].anchor = tmp.anchor;
+			meshes[i].font = tmp.font;
+			meshes[i].fontSize = tmp.fontSize;
+			meshes[i].fontStyle = tmp.fontStyle;
+			meshes[i].characterSize = tmp.characterSize;
+			meshes[i].text = menuItems[i];
+			meshes[i].color = Color.gray;
+			offset -= 1.5f;
+		}
+		meshes [selectedIndex].color = Color.white;
+		//meshes [0].color = Color.green;
+		//meshes [1].color = Color.white;
+		//meshes [2].color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void menuControl() {
+		meshes [selectedIndex].color = Color.gray;
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			executeAction ();
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			selectedIndex = (selectedIndex == (menuItems.Length - 1) ? selectedIndex = 0 : ++selectedIndex);
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			selectedIndex = (selectedIndex == 0 ? menuItems.Length - 1 : --selectedIndex);
+		} else if (Input.GetKeyDown (KeyCode.V)) {
+			print ("selected: " + selectedIndex);
+		}
+		meshes [selectedIndex].color = Color.white;
+	}
+
+	private void executeAction() {
+		print (menuItems [selectedIndex]);
+		if (menuItems [selectedIndex].ToLower ().Equals ("start")) {
+			Application.LoadLevel (gameName);
+		} else if (menuItems [selectedIndex].ToLower ().Equals ("exit")) {
+			Application.Quit();
+		}
+	}
+	void TestFunc() {
+		print ("testfunc");
+		Application.LoadLevel("game");
+	}
+}
