c6efd062ff2517e123e75b87dc7852c2208512d1
Fatumner <Fatumner@wp.pl>
2014-12-05 00:27:15 +0100

    added bobsleigh with wheel colliders and modified controller

---
+++ Assets/scripts/BobsleighController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BobsleighController : MonoBehaviour {
+	
+	public WheelCollider WheelFR;
+	public WheelCollider WheelFL;
+	
+	int steer_max = 10;
+	private float steer = 0.0f;
+	
+	void Update()
+	{
+		steer = Mathf.Clamp(Input.GetAxis("Horizontal"), -1, 1);
+		WheelFL.steerAngle = steer_max * steer;
+		WheelFR.steerAngle = steer_max * steer;
+	}
+}
