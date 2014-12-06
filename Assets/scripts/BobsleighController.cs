2b53dd070325403aca5c4279877982de92109b0d
Fatumner <Fatumner@wp.pl>
2014-12-06 19:44:03 +0100

    Deleted unused terrain, modified bobsleigh controller to work with oculus (turning according to movement of oculus Z-axis), added newest road, removed up and down arrows from movement.

--- Assets/scripts/BobsleighController.cs
+++ Assets/scripts/BobsleighController.cs
@@ -8,10 +8,42 @@ public class BobsleighController : MonoBehaviour {
 	
 	int steer_max = 10;
 	private float steer = 0.0f;
-	
+	void Start()
+	{
+		OVRDevice.ResetOrientation();
+	}
 	void Update()
 	{
-		steer = Mathf.Clamp(Input.GetAxis("Horizontal"), -1, 1);
+		if(OVRDevice.IsHMDPresent())
+		{
+			Vector3 p = new Vector3();
+			Quaternion Ovrinput=Quaternion.identity;
+			OVRDevice.GetCameraPositionOrientation (ref p, ref Ovrinput, 0);
+			float ovrZAngle = Ovrinput.eulerAngles.z;
+			if(ovrZAngle < 5.0f || ovrZAngle > 355.0f)
+			{
+				steer = 0.0f;
+			}
+			else if(ovrZAngle < 180.0f)
+			{
+				if(ovrZAngle > 60.0f)
+					steer = -1.0f;
+				else
+					steer = ovrZAngle / -60.0f;
+			}
+			else if(ovrZAngle > 180.0f)
+			{
+				if(ovrZAngle < 300.0f)
+					steer = 1.0f;
+				else
+					steer = (360.0f - ovrZAngle) / 60.0f;
+			}
+		}
+		else
+		{
+			steer = Mathf.Clamp(Input.GetAxis("Horizontal"), -1, 1);
+		}
+
 		WheelFL.steerAngle = steer_max * steer;
 		WheelFR.steerAngle = steer_max * steer;
 	}
