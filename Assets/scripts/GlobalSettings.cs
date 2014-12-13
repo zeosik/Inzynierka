713f8675616143a2fefeab8d098639c224413ab5
Fatumner <Fatumner@wp.pl>
2014-12-13 23:18:29 +0100

    Fixed splat 5 error

--- Assets/scripts/GlobalSettings.cs
+++ Assets/scripts/GlobalSettings.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class GlobalSettings : MonoBehaviour {
 
+	public static Vector3 mainMenuUserPosition;
+	public static Quaternion mainMenuUserRotation;
+
 	// Use this for initialization
 	void Start () {
-	
+		print ("hi");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
