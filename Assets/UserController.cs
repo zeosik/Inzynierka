08c22ef569a13dee5a605290a5ed462aed61579b
Fatumner <Fatumner@wp.pl>
2014-12-14 01:11:39 +0100

    Deleted unused tree (the bad one), added new scene - for choosing maps, added a few textures used in new scene, the scene is working, but isn't connected to menu scene and oculus controller, and some elements are without textures - didn't have unity pro while working on it

---
+++ Assets/UserController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserController : MonoBehaviour {
+
+	float moveSpeed = 0.7f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W))
+			this.transform.position = new Vector3 (this.transform.position.x, 
+			                                       this.transform.position.y, 
+			                                       this.transform.position.z + moveSpeed);
+		if (Input.GetKey (KeyCode.A))
+			this.transform.position = new Vector3 (this.transform.position.x - moveSpeed, 
+			                                       this.transform.position.y, 
+			                                       this.transform.position.z);
+		if (Input.GetKey (KeyCode.S))
+			this.transform.position = new Vector3 (this.transform.position.x, 
+			                                       this.transform.position.y, 
+			                                       this.transform.position.z - moveSpeed);
+		if (Input.GetKey (KeyCode.D))
+			this.transform.position = new Vector3 (this.transform.position.x + moveSpeed, 
+			                                       this.transform.position.y, 
+			                                       this.transform.position.z);
+	}
+}
