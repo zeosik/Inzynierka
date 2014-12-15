81820a05d8083b5331f5ac97d2c82e6dac5e019b
Fatumner <Fatumner@wp.pl>
2014-12-15 16:25:51 +0100

    Working in-game menu with "esc" for toggleMenu, "p" for togglePause

--- Assets/scripts/BobsleighMenuController.cs
+++ Assets/scripts/BobsleighMenuController.cs
@@ -5,28 +5,45 @@ using UnityEngine.UI;
 public class BobsleighMenuController : MonoBehaviour {
 
 	public Button[] menuItems;
+	public string mapChoosingSceneName;
+	static BobsleighMenuController menu;
 
 	bool menuActive = false;
 	int selectedIndex = 0;
 	bool gamePaused = false;
+	int pauseResumeButtonIndex;
 	// Use this for initialization
 	void Start () {
-	
+		menu = gameObject.transform.GetComponent<BobsleighMenuController>();
+		foreach(Button btn in menuItems)
+		{
+			if(btn.name.ToLower().Contains("pause") ||
+			   btn.name.ToLower().Contains("resume"))
+			{
+				break;
+			}
+			++pauseResumeButtonIndex;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (menuActive) 
 		{
+			gamePaused = GameController.isPaused();
+			menuItems[pauseResumeButtonIndex].GetComponentInChildren<Text>().text = (gamePaused ? "resume " : "pause ") + "game";
 			if(Input.GetKeyDown(KeyCode.DownArrow))
 			{
 				selectedIndex = (selectedIndex == (menuItems.Length - 1) ? selectedIndex = 0 : ++selectedIndex);
 				menuItems[selectedIndex].Select();
-			} else if(Input.GetKeyDown(KeyCode.UpArrow))
+				//menuItems[selectedIndex].
+			} 
+			else if(Input.GetKeyDown(KeyCode.UpArrow))
 			{
 				selectedIndex = (selectedIndex == 0 ? menuItems.Length - 1 : --selectedIndex);
 				menuItems[selectedIndex].Select();
-			} else if(Input.GetKeyDown(KeyCode.Return))
+			} 
+			else if(Input.GetKeyDown(KeyCode.Return))
 			{
 				executeAction();
 			}
@@ -34,14 +51,24 @@ public class BobsleighMenuController : MonoBehaviour {
 	}
 	void executeAction()
 	{
-		if (menuItems [this.selectedIndex].name.ToLower ().Contains ("pause") ||
-		    menuItems [this.selectedIndex].name.ToLower ().Contains ("resume"))
+		if (menuItems [selectedIndex].name.ToLower ().Contains ("pause") ||
+		    menuItems [selectedIndex].name.ToLower ().Contains ("resume"))
 		{
 			gamePaused = !gamePaused;
-			if(gamePaused)
-				BobsleighController.pause();
-			else
-				BobsleighController.resume();
+			GameController.togglePauseGame();
+		}
+		else if (menuItems[selectedIndex].name.ToLower().Contains("exit"))
+		{
+			Application.LoadLevel(mapChoosingSceneName);
+		}
+	}
+	public static void toggleMenu()
+	{
+		menu.menuActive = !menu.menuActive;
+		if(menu.menuActive)
+		{
+			menu.selectedIndex = 0;
+			menu.menuItems[menu.selectedIndex].Select();
 		}
 	}
 }
--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -5,9 +5,10 @@ public class GameController : MonoBehaviour {
 
 	private bool paused = false;
 	private bool inMenu = false;
+	static GameController gameController;
 	// Use this for initialization
 	void Start () {
-	
+		gameController = gameObject.transform.GetComponent<GameController>();
 	}
 	
 	// Update is called once per frame
@@ -16,38 +17,43 @@ public class GameController : MonoBehaviour {
 //			Application.LoadLevel("menu");
 //		}
 		if(Input.GetKeyDown(KeyCode.P)) {
-			pauseGame();
+			togglePauseGame();
 		}
 		if(Input.GetKeyDown(KeyCode.Escape)) {
-			enterMenu();
+			toggleMenu();
 		}
 	}
-	void pauseGame()
+	public static void togglePauseGame()
 	{
 		Canvas[] list = FindObjectsOfType<Canvas>();
 		foreach(Canvas item in list)
 		{
 			if(item.tag.Equals("info"))
 			{
-				paused = !paused;
-				item.enabled = paused;
-				if(paused)
+				gameController.paused = !gameController.paused;
+				item.enabled = gameController.paused;
+				if(gameController.paused)
 					BobsleighController.pause();
 				else
 					BobsleighController.resume();
 			}
 		}
 	}
-	void enterMenu()
+	public static bool isPaused()
 	{
+		return gameController.paused;
+	}
+	public static void toggleMenu()
+	{
+		BobsleighMenuController.toggleMenu();
 		Canvas[] list = FindObjectsOfType<Canvas>();
 		foreach(Canvas item in list)
 		{
 			if(item.tag.Equals("menu"))
 			{
-				inMenu = !inMenu;
-				item.enabled = inMenu;
-				pauseGame();	//Delete later on, when pausing is implemented as a new option in menu
+				gameController.inMenu = !gameController.inMenu;
+				item.enabled = gameController.inMenu;
+				//pauseGame();	//Delete later on, when pausing is implemented as a new option in menu
 			}
 		}
 	}
